913d5d704b5c3de00df8e2e240429db0f3755b4d
Fabricio Palavecino <fabricioleonelpalavecino@gmail.com>
2022-11-24 19:11:27 -0300

    improvement cards to show albums

--- client/src/components/AlbumDetails.js
+++ client/src/components/AlbumDetails.js
@@ -12,17 +12,15 @@ const AlbumDetails = ({ id, name, musician, year, url, onDelete, onEdit }) => {
 
 
     return (
-        <div className="item">
+        <div className="ui card">
             <div className="content">
                 <h3 className="ui header">Album's Name: {name} </h3>
-            </div>
-            <div className="content">
                 <h3 className="ui header">Musician: {musician}</h3>
-            </div>
-            <div className="content">
                 <h3 className="ui header">Year of release: {year}</h3>
             </div>
-            <img className="ui image" src={url} alt={name} style={{ marginBottom: '10px'}}/>
+            <div className="image">
+                <img src={url} alt={name} style={{ marginBottom: '10px'}}/>
+            </div>
             <button className="ui positive button"  onClick={() => handleEdit(id)}>Edit</button>
             <button className="ui negative button"  onClick={() => handleDelete(id)} >Delete</button>            
         </div>
